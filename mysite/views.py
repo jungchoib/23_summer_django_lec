7289d0bf145c649ec4445048255f22f25e2e2316
jungchoib <jungchoi1@naver.com>
2023-07-25 22:09:57 +0900

    2week django

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,8 @@
+# from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from .models import MainContent
 def index(request):
-    return HttpResponse("Hello world")
+    # return HttpResponse("Hello world")
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_list.html', context)
