efef6732193547268206b55b39e639aae7928883
jungchoib <jungchoi1@naver.com>
2023-07-25 23:44:28 +0900

    2week final

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.mainpage),
     path('company/', views.company),
+    path('main/', views.mainmain),
+    path('login/', views.login),
+    path('login_new/', views.login_new),
+    path('login_ser/', views.login_search),
+    path('item1/', views.item1),
+    path('item2/', views.item2),
+
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,15 @@ def mainpage(request):
     return render(request, 'pages/mainpage.html')
 def company(request):
     return render(request, 'pages/company_info.html')
+def mainmain(request):
+    return render(request, 'pages/main_mainpage.html')
+def login(request):
+    return render(request, 'pages/logIn_page.html')
+def login_new(request):
+    return render(request, 'pages/logIn_new.html')
+def login_search(request):
+    return render(request, 'pages/logIn_search.html')
+def item1(request):
+    return render(request, 'pages/item1.html')
+def item2(request):
+    return render(request, 'pages/item2.html')
