4f95c2bc6c813e1e431602d0e3b56d28122a5cda
jungchoib <jungchoi1@naver.com>
2023-08-04 16:41:43 +0900

    수정삭제버튼 완료

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', views.index),
     path('<int:content_id>/', views.detail, name='detail'),
     path('main/', views.mainmain),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
